1742ab3df15b57f6ae399458a41d11be5adb9027
saku321 <38668881+saku321@users.noreply.github.com>
2022-10-26 19:14:41 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import { io } from "socket.io-client";
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+const socket = io("http://localhost:3002");
+function App() {
+
+
+    const [user, setUser] = useState("user32");
+    const [alertStatus, setAlertStatus] = useState(false);
+    const [timer, setTimer] = useState("0:00");
+    const [players, setPlayers] = useState([]);
+    const [winnerTxt, setWinnerTxt] = useState("");
+
+    useEffect(() => {
+        socket.on("connect", ()=> {
+            console.log("connected");
+        });
+        socket.on("timerCount", (data) =>{
+            setTimer(`${data.min}:${data.sec}`);
+        });
+        socket.on("winner", (data) => {
+            setWinnerTxt(data.data);
+            setAlertStatus(true);
+            setTimeout(() => {
+                setAlertStatus(false);
+            },5000);
+        });
+        socket.on("players", (data) => {
+            setPlayers(data.players);
+        });
+    });
+    const joinGame = () => {
+        
+        const currentUser = user.slice(0, 5).toString();
+        socket.emit("joinRoom", currentUser);
+    }
+    
+  return (
+      <div className="App">
+          {alertStatus && (
+              <Alert variant="primary" style={{ textAlign: "center", fontSize: "25px" }}>Arvonnan voittaja: {winnerTxt}</Alert>
+          )}
+          <div id="centerDiv">
+
+              <h1>Arvonta</h1>
+              <h2>{timer}</h2>
+              <InputGroup className="mb-3">
+                  <InputGroup.Text id="inputGroup-sizing-default">
+                      Nimi
+                  </InputGroup.Text>
+
+              
+              <Form.Control className="nameInput"
+                  aria-label="Default"
+                      aria-describedby="inputGroup-sizing-default" onChange={(val: any) => setUser(val.target.value)}
+                  /></InputGroup>
+             <br></br>
+              <Button variant="success" id="joinBtn" onClick={joinGame}>Osallistu arvontaan</Button>
+              
+              <div id="playerList">
+                  <p>Osallistujat</p>
+                  <ul>
+
+                  {players.map((player) =>
+                      <li>{player} </li> 
+                  ) }
+                  </ul>
+
+              </div>
+            </div>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/arvontaSystem.js
@@ -0,0 +1,62 @@
+﻿const { Server } = require("socket.io");
+
+const io = new Server(3002, { perMessageDeflate: false, cors: { origin: ["http://localhost:3000"] }, });
+let time = 20;
+
+let names = [];
+//kello
+const timerFunction = () => {
+    const timer = setInterval(() => {
+        let min = Math.floor(time / 60);
+        let sec = time % 60;
+
+        sec = sec < 10 ? "0" + sec : sec;
+        time--;
+
+        io.sockets.emit('timerCount', { min: min, sec: sec });
+
+
+        if (min == 0 && sec == 0) {
+            
+
+           
+
+            var pickWinner = Math.floor(Math.random() * names.length);
+
+            
+
+            io.sockets.emit("winner", { data: names[pickWinner] });
+
+            clearInterval(timer);
+            names = []
+            io.sockets.emit("players", { players: names });
+
+        }
+
+    }, 1000);
+}
+
+io.on("connection", (socket) => {
+    socket.on("joinRoom", (name) => {
+        //jos nimi on listassa ei lisätä uudelleen
+        if (names.includes(name)) {
+            
+
+        } else {
+            names.push(name);
+
+
+        }
+        //näyttää pelaajat sivustolle
+        io.sockets.emit("players", { players: names });
+
+        
+        //jos osallistujia on enemmän kuin 2 niin aloita peli
+        
+        if (names.length ==2) {
+            timerFunction();
+
+        }
+    });
+    
+});
